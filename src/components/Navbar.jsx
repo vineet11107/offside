b82183bb47c68bfd9b831d390eadbabe1d24fd7d
Vineet Pandey <vineetjr.11107@gmail.com>
2025-05-05 15:00:07 +0530

    updated navbar issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,82 @@
-import React from "react";
+// Navbar.jsx
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => setDrawerOpen(!drawerOpen);
+  const closeDrawer = () => setDrawerOpen(false);
+
   return (
-    <nav className="navbar">
-      <img src="/assets/logo_Offside.jpeg" alt="Logo" className="logo" />
-      <ul className="nav-links">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/services">Services</Link>
-        </li>
-        <li>
-          <Link to="/gallery">Gallery</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      {/* Hamburger Icon */}
+      {!drawerOpen && (
+        <div className="hamburger" onClick={toggleDrawer}>
+          &#9776;
+        </div>
+      )}
+
+      {/* Overlay */}
+      {drawerOpen && <div className="overlay" onClick={closeDrawer}></div>}
+
+      {/* Side Drawer */}
+      <nav className={`sidebar ${drawerOpen ? "open" : ""}`}>
+        <button className="close-btn" onClick={closeDrawer}>
+          &times;
+        </button>
+        <ul className="nav-links">
+          <li>
+            <Link to="/" onClick={closeDrawer}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" onClick={closeDrawer}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/services" onClick={closeDrawer}>
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link to="/gallery" onClick={closeDrawer}>
+              Gallery
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" onClick={closeDrawer}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* Desktop Navbar */}
+      <nav className="navbar">
+        <img src="/assets/logo_Offside.jpeg" alt="Logo" className="logo" />
+        <ul className="nav-links desktop">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/services">Services</Link>
+          </li>
+          <li>
+            <Link to="/gallery">Gallery</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 }
 
